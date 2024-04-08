d3fc77bcd6fc89f05a85840a466ef807ff121ce3
Your Name <you@example.com>
2024-04-08 14:58:43 -0300

    sanidade negativa

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function menuBurger() {
+    console.log("chamamos links")
+
+    let burger = document.menu;
+
+
+    if (burger.style.display === "none") {
+
+      burger.style.display = "block";
+
+    }
+     else {
+
+      burger.style.display = "none";
+    }
+  }
